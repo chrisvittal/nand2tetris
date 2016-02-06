11f63733cb7bbee780417b75071320af989a7e43
chrisvittal <christopher.vittal@gmail.com>
2016-02-06 18:54:07 -0500

    Started Project 4. Finished Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Initialize:
+  @1
+  D=M
+  @3
+  M=D
+  @2
+  M=0
+  @3
+  D=M
+(LOOP)
+  @END
+  D;JEQ
+  @0
+  D=M
+  @2
+  M=D+M
+  @3
+  MD=M-1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+  
